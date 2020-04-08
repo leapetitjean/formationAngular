5a6cb72baefdcfb40680414762cb00c61333e8f7
Lea Petit-Jean Genat <leapetit-jean@orange.fr>
2020-04-08 10:25:43 +0200

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -56,6 +57,7 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
+    MatProgressSpinnerModule,
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
